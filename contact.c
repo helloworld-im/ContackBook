4f8eaf172e7070a5cfbd5105e3c22bc5fbefd48c
Cooper Dev <72105974+helloworld-im@users.noreply.github.com>
2022-10-01 15:41:48 +0530

    Create contact.c

---
+++ contact.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <conio.h>
+# include <string.h>
+# include <stdlib.h>
+void menu();
+void adddetails();
+void searchdetails();
+void modifydetails();
+void deletedetails();
+void display();
